cd3947b6cb069fb7dcb1d20607ef8ce6a919ae99
Jabbwa <sopida_kit@cmu.ac.th>
2022-02-01 21:46:22 +0700

    complete

--- lab21_1.cpp
+++ lab21_1.cpp
@@ -5,8 +5,54 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect R1, Rect R2){
+	double w,h;
+	if (R1.x + R1.w > R2.x)
+	{
+		if (R1.x > R2.x)
+		{
+			w = R1.w - (R1.x - R2.x);
+		}else w = R1.w - (R2.x - R1.x);
+			if (w > R1.w) w = R1.w;
+			else if (w > R2.w) w = R2.w;
+			else w = w;
+		
+		if (R1.y > R2.y)
+		{
+			h = R1.h - (R1.y - R2.y);
+		}else h = R1.h - (R2.y - R1.y);
+			if (h > R1.h) h = R1.h;
+			else if(h > R2.h) h = R2.h;
+			else h = h;
+		
+	}else return 0;
 
-
-
+	return w*h;
+	
 }
+
+int main(){
+Rect R1 = {1,1,5,5};
+Rect R2 = {2,2,5,5};	
+cout << overlap(R1,R2);			
+	return 0;
+}
+
+
+/*Rect lap;
+	if (R1.x + R1.w < R2.x + R2.w && R2.x > R1.x && )
+	{
+		lap.w = R1.w - (R2.x-R1.x);
+		lap.h = R1.h - (R2.y-R1.y);
+		return lap.w*lap.h;
+	}else if (R1.x + R1.w > R2.x + R2.w && R2.x > R1.x && R1.w - R1.x > R2.x - R2.w)
+	{
+		return R2.w*R2.h;
+	}else if ((R2.x+R2.w) > (R1.x+R1.w))
+	{
+		return 0;
+	}
+	
+
+	
+	return 0; */
